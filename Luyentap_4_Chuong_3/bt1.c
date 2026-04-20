6975ba8d0daf330de1f31000263037d180061023
hieubui1110 <hiruduongtham@gmail.com>
2026-04-21 02:03:47 +0700

    chuong 7 luyen tap 5

--- Luyentap_4_Chuong_3/bt1.c
+++ Luyentap_4_Chuong_3/bt1.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
-    printf("Nhap 3 so a, b, c: ");
+    int a, b, c, min;
+
+    printf("Nhap 3 so nguyen a, b, c: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    int min = a;
-    if (b < min) min = b;
-    if (c < min) min = c;
+    if (a <= b && a <= c) {
+        min = a;
+    } else if (b <= a && b <= c) {
+        min = b;
+    } else {
+        min = c;
+    }
 
-    printf("Gia tri nho nhat la: %d", min);
+    printf("Gia tri nho nhat la: %d\n", min);
+
     return 0;
 }
